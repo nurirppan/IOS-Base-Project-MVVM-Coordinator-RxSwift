7c7e100f724090ada43522af6516249b22e98a47
Nur Irfan Pangestu <nur.pangestu@indivaragroup.com>
2020-11-24 10:38:54 +0700

    create project

---
+++ IOS Base Project MVVM Coordinator RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS Base Project MVVM Coordinator RxSwift
+//
+//  Created by Nur Irfan Pangestu on 24/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOS Base Project MVVM Coordinator RxSwiftTests/IOS_Base_Project_MVVM_Coordinator_RxSwiftTests.swift
@@ -0,0 +1,33 @@
+//
+//  IOS_Base_Project_MVVM_Coordinator_RxSwiftTests.swift
+//  IOS Base Project MVVM Coordinator RxSwiftTests
+//
+//  Created by Nur Irfan Pangestu on 24/11/20.
+//
+
+import XCTest
+@testable import IOS_Base_Project_MVVM_Coordinator_RxSwift
+
+class IOS_Base_Project_MVVM_Coordinator_RxSwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
